514a80a7ee18463b15372d4d3d6f4f402844db69
Giane-QUT <n10532935@qut.edu.au>
2021-10-28 16:07:41 +1000

    Added table for tweet display

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const natural = require('natural');
 const router = express.Router();
 
+
 const twtCreds = require('../my-twitter-keys.json');
 const twtBearer = twtCreds.bearer_token;
 
@@ -30,7 +31,7 @@ router.get('/', function(req, res, next) {
         let tweets = data.statuses;
         parseTweets(tweets);
         console.log("Query received>>" + req.query.tweetFilters + "<<");
-        res.render('index', { title: 'Tweet Analyser', opti: "AN option" });
+        res.render('index', { title: 'Tweet Analyser', tweets_data: parseTweets(tweets)});
       })
       .catch((e) => {
         console.error(e);
@@ -46,13 +47,32 @@ router.get('/', function(req, res, next) {
   }
 });
 
-function parseTweets(tweets) {
-  for (let tweet of tweets) {
-    console.log("Username is<<: " + tweet.user.screen_name);
-    console.log("Tweet is>>>> " + tweet.text + "\n");
-    console.log("Sentiment is====");
-    console.log(analyzer.getSentiment(tokenizer.tokenize(tweet.text)));
+
+
+function parseTweets(tweet_data) {
+  tweets = []
+  for (let theTweet of tweet_data) {
+    // Populate tweet obj to be added to tweet array
+    tweet = {
+      user: "",
+      text: "",
+      date: "",
+      sentimentVal: 0,
+    }
+    tweet.user = theTweet.user.screen_name;
+    tweet.text =  theTweet.text;
+    tweet.date = theTweet.created_at;
+    tweet.sentimentVal = analyzer.getSentiment(tokenizer.tokenize(theTweet.text))
+    tweets.push(tweet);
+    //console.log(tweet);
+    // console.log("Username is<<: " + tweet.user.screen_name);
+    // console.log("Tweet is>>>> " + tweet.text + "\n");
+    // console.log("Sentiment is====");
+    //console.log(analyzer.getSentiment(tokenizer.tokenize(tweet.text)));
+  
   }
+  console.log(tweets)
+  return tweets;
 }
 
 module.exports = router;
