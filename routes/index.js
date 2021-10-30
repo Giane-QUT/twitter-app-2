e1da3441cc366ef7b1d4e0ec3b86fcc5924f8769
Giane-QUT <n10532935@qut.edu.au>
2021-10-31 01:42:19 +1000

    Added S3 saving functionality

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const axios = require('axios');
 const natural = require('natural');
 const router = express.Router();
 const {promisify} = require('util');
+const AWS = require('aws-sdk');
+require('dotenv').config();
 
 // Twitter setup
 const twtCreds = require('../my-twitter-keys.json');
@@ -14,9 +16,27 @@ const client = redis.createClient();
 client.on('error', (err) => {
   console.log("Error " + err);
 });
-
+// Promisify REDIS callback functions
 const getRedis = promisify(client.get).bind(client);
 const saveRedis = promisify(client.setex).bind(client);
+function saveToRedis(query,data) {
+  return saveRedis(`tweetFilter:${query}`, 300, JSON.stringify({source:'Redis Cache',...data}))
+}
+
+// AWS S3 Setup
+AWS.config.getCredentials((err) => {
+  if (err) {
+    console.log(err.stack);
+  }
+  else {
+
+  }
+})
+const bucketName = 'n10532935-twitterbucket';
+const s3 = new AWS.S3({apiVersion:'2006-03-01'});
+function saveToS3(query,data) {
+  return s3.putObject({Bucket:bucketName, Key:query, Body:data}).promise();
+}
 
 // Use of Natural for sentiment analysis was found
 // https://blog.logrocket.com/natural-language-processing-for-node-js/
@@ -26,46 +46,66 @@ const stemmer = natural.PorterStemmer;
 const analyzer = new Analyzer("English", stemmer, "afinn")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
+  // NOTE by Giane: Minimal error handling is currently used
   if (req.query.tweetFilters) {
     try {
       const query = req.query.tweetFilters;
       // Start storage checks before calling twitter api
-      getRedis(`tweet:${query}`)
+      getRedis(`tweetFilter:${query}`)
       .then((result) => {
+        let foundS3 = false;
         if (result) {
           console.log("Found key" );
           const resultJSON = JSON.parse(result);
           return resultJSON;
         }
-        // else if (AMAZON S3 Bucket) {
-        //
-        // }
         else {
-          return (
-            axios.get(`https://api.twitter.com/1.1/search/tweets.json?q=${query}&lang=en`,{headers: {Authorization: 'Bearer ' + twtBearer}})
-            .then((response) => {
-              if (response.status == 200) {
-                return response.data;
+          // Check bucket if no redis matches
+          const params = {Bucket: bucketName, Key:`tweetFilter-${query}`}
+          return(
+            s3.getObject(params).promise()
+            .then((result) =>{
+              if (result) {
+                const resultJSON = JSON.parse(result.Body);
+                return resultJSON;
               }
-              else throw Error("Status code " + response.status + " was received.")
             })
-            .then()
+            .catch((err) => {
+              console.error(err);
+              if (err.statusCode === 404) {
+                return (
+                  axios.get(`https://api.twitter.com/1.1/search/tweets.json?q=${query}&lang=en`,{headers: {Authorization: 'Bearer ' + twtBearer}})
+                  .then((response) => {
+                    if (response.status == 200) {
+                      return response.data;
+                    }
+                    else throw Error("Status code " + response.status + " was received.")
+                  })
+                )
+              }
+            }) // end then clause
           )
-        }
+        } // end check s3 and api
       })
       .then((data) => {
         // Iterate and get the tweets' text
-        console.log(data)
+        //console.log(data)
         let tweets = data.statuses;
         console.log("Query received>>" + req.query.tweetFilters + "<<");
         res.render('index', { title: 'Tweet Analyser', tweets_data: parseTweets(tweets)});
         return data
       })
       .then((data) => {
-        Promise.all([saveRedis(`tweet:${query}`, 300, JSON.stringify({source:'Redis Cache',...data}))])
-        .then(() => {
-          return "Saved"
+        const body = JSON.stringify({source:'S3 Bucket',...data})
+        Promise.all([saveToRedis(query,data),saveToS3(query,body)])
+        .then((results) => {
+          console.log("Saved to REDIS? " + results[0]);
+          console.log("Saved to S3? " + results[1]);
+        })
+        .catch((err) => {
+          console.log("Saving err");
+          console.error(err);
         })
       })
       .catch((err) => {
@@ -104,30 +144,7 @@ function parseTweets(tweet_data) {
     //console.log(analyzer.getSentiment(tokenizer.tokenize(tweet.text)));
   
   }
-  console.log(tweets)
   return tweets;
-}
-
-// Checks redis for given query
-function checkRedis(query) {
-  console.log("Checking redis for" + query)
-  return client.get(`tweet:${query}`,(err, result) => {
-    console.log("Result is" + result);
-    if (result) {
-      const resultJSON = JSON.parse(result);
-      console.log(resultJSON);
-      return resultJSON;
-    } else {
-      console.log("RETURNING 0");
-      return 0;
-    }
-  })
-}
-
-// Save data on query to local Redis for 5 minutes
-function saveToRedis(query,data) {
-  console.log("Saved to redis query " + query);
-  client.setex(`tweet:${query}`, 300, JSON.stringify({source:'Redis Cache',...data}));
 }
 
 module.exports = router;
